7769694e9e9b42e5b4810b4c206a99924752b45a
LxZhusdu <153404435+LxZhusdu@users.noreply.github.com>
2023-12-12 11:43:10 +0800

    Update plot_LinfKHDI.R

--- plot_LinfKHDI.R
+++ plot_LinfKHDI.R
@@ -21,25 +21,25 @@ par(oma=c(0,0,0,0),mar=c(4,4,0.5,1),cex=0.8,las=1,mgp=c(2.5,1,0))
 plotCI(x,Mean,ui=Upper,li=Lower,ylim=c(16,32), xlim=c(1993,2018),gap=0.02,
     pch=20,cex=1,xlab="Year",ylab=bquote(italic(L[infinity])~(cm)),col="red",bty="l",tck=-0.03)
      
-points(rep(1993,1),b[1],pch=1,col="black",cex=0.8)   #chenzuozhi
-points(rep(1995,1),b[2],pch=5,col="cyan",cex=0.8)   #ZHANGKUI
-points(rep(1997,1),b[3],pch=4,col="blue",cex=0.8)   #yuan weiwen
-points(rep(1998,1),b[4],pch=5,col="cyan",cex=0.8)    #ZHANGKUI
-points(rep(1999,1),b[5],pch=1,col="black",cex=0.8)   #chenzuozhi
-points(2006,b[6],pch=5,col="cyan",cex=0.8)  # zhangkui
-points(2006,b[7],pch=6,col="blue",cex=0.8)  # wangxuehui
-points(2007,b[8],pch=6,col="black",cex=0.8)  # Wangxuehui------
+points(rep(1993,1),b[1],pch=1,col="black",cex=0.8)   
+points(rep(1995,1),b[2],pch=5,col="cyan",cex=0.8)   
+points(rep(1997,1),b[3],pch=4,col="blue",cex=0.8)   
+points(rep(1998,1),b[4],pch=5,col="cyan",cex=0.8)    
+points(rep(1999,1),b[5],pch=1,col="black",cex=0.8)   
+points(2006,b[6],pch=5,col="cyan",cex=0.8)  
+points(2006,b[7],pch=6,col="blue",cex=0.8)  
+points(2007,b[8],pch=6,col="black",cex=0.8)  
 points(2007,b[9],pch=6,col="blue",cex=0.8)  
-points(2008,b[10],pch=17,col="green",cex=0.8)  #hougang
-points(2008,b[11],pch=0,col="black",cex=0.8)   # Yanyunrong
-points(2009,b[12],pch=6,col="blue",cex=0.8)  # wang
-points(2010,b[13],pch=6,col="blue",cex=0.8)  # wang
+points(2008,b[10],pch=17,col="green",cex=0.8)  
+points(2008,b[11],pch=0,col="black",cex=0.8)   
+points(2009,b[12],pch=6,col="blue",cex=0.8)  
+points(2010,b[13],pch=6,col="blue",cex=0.8)  
 points(2011,b[14],pch=6,col="blue",cex=0.8)
-points(2012,b[15],pch=6,col="blue",cex=0.8)  # wang
+points(2012,b[15],pch=6,col="blue",cex=0.8)  
 points(2013,b[16],pch=6,col="blue",cex=0.8)
-points(2014,b[17],pch=6,col="blue",cex=0.8)  # wang
-points(2015,b[18],pch=6,col="blue",cex=0.8)  # wang
-points(2015,b[19],pch=5,col="cyan",cex=0.8)  # zhangkui
+points(2014,b[17],pch=6,col="blue",cex=0.8)  
+points(2015,b[18],pch=6,col="blue",cex=0.8)  
+points(2015,b[19],pch=5,col="cyan",cex=0.8)  
 points(2016,b[20],pch=6,col="blue",cex=0.8)
 points(2018,b[21],pch=6,col="blue",cex=0.8)
 text(1993.1,28,"1961",cex=0.6)
@@ -48,25 +48,25 @@ text(1997,22.4,"1992-1993",cex=0.6)
 plotCI(x,Mean2,ui=Upper2,li=Lower2,ylim=c(0,1), xlim=c(1993,2018),gap=0.02,
     pch=20,cex=1,xlab="Year",ylab=bquote(italic(K)~(year^{-1})),col="red",bty="l",tck=-0.03)
 
-points(rep(1993,1),a[1],pch=1,col="black",cex=0.8)   #chenzuozhi
-points(rep(1995,1),a[2],pch=5,col="cyan",cex=0.8)   #ZHANGKUI
-points(rep(1997,1),a[3],pch=4,col="blue",cex=0.8)   #yuan weiwen
-points(rep(1998,1),a[4],pch=5,col="cyan",cex=0.8)    #ZHANGKUI
-points(rep(1999,1),a[5],pch=1,col="black",cex=0.8)   #chenzuozhi
-points(2006,a[6],pch=5,col="cyan",cex=0.8)  # zhangkui
-points(2006,a[7],pch=6,col="blue",cex=0.8)  # wangxuehui
-points(2007,a[8],pch=6,col="black",cex=0.8)  # Wangxuehui------
+points(rep(1993,1),a[1],pch=1,col="black",cex=0.8)   
+points(rep(1995,1),a[2],pch=5,col="cyan",cex=0.8)   
+points(rep(1997,1),a[3],pch=4,col="blue",cex=0.8)  
+points(rep(1998,1),a[4],pch=5,col="cyan",cex=0.8)    
+points(rep(1999,1),a[5],pch=1,col="black",cex=0.8)  
+points(2006,a[6],pch=5,col="cyan",cex=0.8)  
+points(2006,a[7],pch=6,col="blue",cex=0.8)  
+points(2007,a[8],pch=6,col="black",cex=0.8)  
 points(2007,a[9],pch=6,col="blue",cex=0.8)  
-points(2008,a[10],pch=17,col="green",cex=0.8)  #hougang
-points(2008,a[11],pch=0,col="black",cex=0.8)   # Yanyunrong
-points(2009,a[12],pch=6,col="blue",cex=0.8)  # wang
-points(2010,a[13],pch=6,col="blue",cex=0.8)  # wang
+points(2008,a[10],pch=17,col="green",cex=0.8) 
+points(2008,a[11],pch=0,col="black",cex=0.8)  
+points(2009,a[12],pch=6,col="blue",cex=0.8)  
+points(2010,a[13],pch=6,col="blue",cex=0.8)  
 points(2011,a[14],pch=6,col="blue",cex=0.8)
-points(2012,a[15],pch=6,col="blue",cex=0.8)  # wang
+points(2012,a[15],pch=6,col="blue",cex=0.8)  
 points(2013,a[16],pch=6,col="blue",cex=0.8)
-points(2014,a[17],pch=6,col="blue",cex=0.8)  # wang
-points(2015,a[18],pch=6,col="blue",cex=0.8)  # wang
-points(2015,a[19],pch=5,col="cyan",cex=0.8)  # zhangkui
+points(2014,a[17],pch=6,col="blue",cex=0.8)  
+points(2015,a[18],pch=6,col="blue",cex=0.8)  
+points(2015,a[19],pch=5,col="cyan",cex=0.8)  
 points(2016,a[20],pch=6,col="blue",cex=0.8)
 points(2018,a[21],pch=6,col="blue",cex=0.8)
 lines(c(1961,seq(1999,2018,1)),rep(0.267,times=21),col="gray",lty=3)
